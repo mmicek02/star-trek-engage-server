4446325a7b119429cbb1560a4efd0d805a8149ea
mmicek02 <micekmichael@gmail.com>
2020-03-03 16:43:51 -0600

    added https:// to CLIENT ORIGIN

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/startrekengage',
-    CLIENT_ORIGIN: 'star-trek-engage-client-app.now.sh',
+    CLIENT_ORIGIN: 'https://star-trek-engage-client-app.now.sh',
 }
